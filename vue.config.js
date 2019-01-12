59694a7f66b063f43bdde73fc5325fd261febdca
aiyk <aiyk.ekwe@gmail.com>
2019-01-12 10:32:16 +0100

    vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    css: {
+        loaderOptions: {
+            css: {
+                // options here will be passed to css-loader
+                extract: false,
+            },
+            postcss: {
+                // options here will be passed to postcss-loader
+            }
+        }
+    }
+}
