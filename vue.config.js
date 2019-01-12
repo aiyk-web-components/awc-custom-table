067082210cc02865396c820e963d72836cd8e9ce
aiyk <aiyk.ekwe@gmail.com>
2019-01-12 19:02:13 +0100

    ...

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,5 @@ const path = require("path");
 module.exports = {
     css: {
         extract: false
-    },
-    pluginOptions: {
-        "style-resources-loader": {
-            preProcessor: "scss",
-            patterns: [path.resolve(__dirname, "./src/style.scss")]
-        }
     }
 };
