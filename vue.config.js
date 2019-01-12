d1cf8dcd23b53739da93cf2c0b3443fbab711c40
aiyk <aiyk.ekwe@gmail.com>
2019-01-12 19:05:48 +0100

    npm deployable

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,11 @@ const path = require("path");
 module.exports = {
     css: {
         extract: false
+    },
+    pluginOptions: {
+        "style-resources-loader": {
+            preProcessor: "scss",
+            patterns: [path.resolve(__dirname, "./src/style.scss")]
+        }
     }
 };
